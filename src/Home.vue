ff675f97c17e3b9655b86927ef2fc1bb19a683ec
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-01 01:30:13 +0500

    intial commit

---
+++ src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <app-title v-for="title in 5"></app-title>
+  </div>
+</template>
+<script>
+  //local component
+  import PageTitle from "./PageTitle";
+  export default {
+    components: {
+      'app-title': PageTitle
+    },
+  }
+</script>
---
+++ src/PageTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--  one root element -->
+  <div>
+    <h1>Page Title: {{title}}</h1>
+    <hr>
+    <button @click="changeTitle">Change Title</button>
+  </div>
+  <!--  one root element -->
+</template>
+<script>
+  // data as function so not conflict with data property of vue instance
+  export default {
+    data: function () {
+      return {title: 'Welcome to Homepage'}
+    },
+    methods:{
+      changeTitle(){
+        this.title = 'Welcome again';
+      }
+    },
+  }
+</script>
