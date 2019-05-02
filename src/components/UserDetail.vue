7158a23b702b5da88676bb39c6e7ce111444a33e
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-02 14:50:20 +0500

    passing data from parent component to child component

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>Username: {{name}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    // for transferring data from PARENT to CHILD we use PROPS(property set from outside i.e PARENT) property
+    props: ['name']
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>
