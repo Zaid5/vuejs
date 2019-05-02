8f8a0d07205a2aed5637c992b3dc8ba5480daced
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-02 14:57:11 +0500

    to clean master branch added all files in temp branch

---
+++ (src)7.section/main.js
@@ -0,0 +1,19 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+// import Home from "./Home";
+//
+// //global component
+// Vue.component('ztitle', Home);
+//
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// });
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-// import Home from "./Home";
-//
-// //global component
-// Vue.component('ztitle', Home);
-//
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// });
-
 import Vue from 'vue'
 import App from './App.vue'
 
