ecaee0a010331a18b6751240000a640b07f9c756
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-02 15:38:21 +0500

    can use props property as normal in whole vue object

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,14 +2,20 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>Username: {{name}}</p>
+<!--    <p>Username: {{myName}}</p>-->
+    <p>Username: {{switchName()}}</p>
   </div>
 </template>
 
 <script>
   export default {
     // for transferring data from PARENT to CHILD we use PROPS(property set from outside i.e PARENT) property
-    props: ['name']
+    props: ['myName'],
+    methods:{
+      switchName(){
+        return this.myName.split("").reverse().join("");
+      }
+    }
   }
 </script>
 
