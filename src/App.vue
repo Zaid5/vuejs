5f86f4ef9a3ef277676d18657b8ff399cc349315
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-03 18:02:00 +0500

    using slot to render data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <appQuote quote="a wonderful day"></appQuote>
+        <appQuote>
+          <h1>the message</h1>
+          <p>a wonderful day</p>
+        </appQuote>
       </div>
     </div>
   </div>
