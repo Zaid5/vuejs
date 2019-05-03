9a0828f700c3aba966228046b2bfccc259c07805
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-03 17:55:15 +0500

    chapter 8

---
+++ src.8.mycoding/components/UserDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>Username: {{myName}}</p>
+    <p>Age: {{userAge}}</p>
+<!--    <p>Username: {{switchName()}}</p>-->
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn">p-resetFn Name</button>
+  </div>
+</template>
+
+<script>
+  import {eventBus} from "../main";
+
+  export default {
+    // for transferring data from PARENT to CHILD we use PROPS(property set from outside i.e PARENT) property
+    props: {
+      myName:{
+        type:String,
+      },
+      resetFn: Function,
+      userAge: Number
+    },
+    methods:{
+      // switchName(){
+      //   return this.myName.split("").reverse().join("");
+      // },
+      resetName(){
+        this.myName = 'zaidu';
+        // this.$emit("nameWasChanged", this.myName);
+      },
+    },
+    created() {
+      eventBus.$on('ageWasChanged', (age) => {
+        this.userAge = age;
+      });
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>
