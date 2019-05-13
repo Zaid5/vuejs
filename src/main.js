f6e0efe84cbc97596085dc49d7df969477afbb0c
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 12:59:05 +0500

    working with custom directives

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+Vue.directive('highlight',{
+  bind(el,binding,vnode){
+    // el.style.backgroundColor = 'lightgreen';
+    el.style. backgroundColor = binding.value;  //user determine the bg color
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
