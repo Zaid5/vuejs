7b5998882d45b9f7e81e8fcf12bb70dbaf5f1fad
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 13:16:35 +0500

    working with custom directives with custom args

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import App from './App.vue'
 Vue.directive('highlight',{
   bind(el,binding,vnode){
     // el.style.backgroundColor = 'lightgreen';
-    el.style. backgroundColor = binding.value;  //user determine the bg color
+    // el.style. backgroundColor = binding.value;  //user determine the bg color
+    if (binding.arg == 'background') {
+      el.style.backgroundColor = binding.value;
+    }
+    else{
+        el.style.color = binding.value;
+    }
   }
 });
 
