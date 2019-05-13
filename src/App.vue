de9618df7d42ca3c2265a1707294e692a7d0c2cc
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 12:47:26 +0500

    working with built-in directives

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some text here'"></p>
+        <p v-html="'<strong>Some text here</strong>'"></p>
+      </div>
     </div>
+    <hr>
+  </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
-
-    export default {
-        components: {
-            appUser: User
-        }
-    }
+  export default {
+  }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+
 </style>
