6bfcf74e1313af2afe422b76a7797d10dd1f724f
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 14:37:45 +0500

    working with local directives and multiple modifiers

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed="'red'">Color this one too</p>
+        <p v-local-highlight:background.delayed.blink="'red'">Color this one too</p>
       </div>
     </div>
   </div>
@@ -27,14 +27,33 @@
           if (binding.modifiers['delayed']){
             delay =3000;
           }
-          setTimeout(()=>{
-            if (binding.arg == 'background') {
-              el.style.backgroundColor = binding.value;
-            }
-            else{
-              el.style.color = binding.value;
-            }
-          },delay);
+          if (binding.modifiers['blink']){
+            let mainColor = binding.value;
+            let secondColor = 'blue';
+            let currentColor = mainColor;
+            setTimeout(()=>{
+              setInterval( ()=>{
+                currentColor == secondColor? currentColor = mainColor : currentColor = secondColor
+                if (binding.arg == 'background') {
+                  el.style.backgroundColor = currentColor;
+                }
+                else{
+                  el.style.color = currentColor;
+                }
+              },delay);
+              },1000 );
+
+          }
+          else {
+            setTimeout(()=>{
+              if (binding.arg == 'background') {
+                el.style.backgroundColor = binding.value;
+              }
+              else{
+                el.style.color = binding.value;
+              }
+            },delay);
+          }
         }
       }
     }
