6f1a36fbfe0f6250c91963cc684f3ce223cae32f
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 15:45:56 +0500

    added local filters

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{text | toUppercase}}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
-
-    export default {
-        components: {
-            appUser: User
-        }
+  export default {
+    data(){
+      return{
+        text: 'hello world'
+      }
+    },
+    filters:{
+      'toUppercase'(value){
+        return value.toUpperCase();
+      }
     }
+  }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+
 </style>
