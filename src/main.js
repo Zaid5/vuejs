d6e204b992eb6d6523ed230d89476438a24a0cae
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-13 14:12:34 +0500

    working with custom directives with custom modifiers

--- src/main.js
+++ src/main.js
@@ -6,12 +6,19 @@ Vue.directive('highlight',{
   bind(el,binding,vnode){
     // el.style.backgroundColor = 'lightgreen';
     // el.style. backgroundColor = binding.value;  //user determine the bg color
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
+    var delay=0;
+    if (binding.modifiers['delayed']){
+      delay =3000;
     }
-    else{
+    setTimeout(()=>{
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      }
+      else{
         el.style.color = binding.value;
-    }
+      }
+    },delay);
+
   }
 });
 
