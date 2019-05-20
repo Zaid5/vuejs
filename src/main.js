b59bb005497041db6c6b533e18f3ee2dc6d6e7b5
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-20 14:46:16 +0500

    added response interceptors

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import response from "vue-resource/src/http/response";
 
 Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vue-server-2f3e6.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => {
+      return {messages: response.body}
+    }
+  });
+});
 
 new Vue({
   el: '#app',
