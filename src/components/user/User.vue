908f3409a90c824f19acf216dbc2be8b6959591b
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-21 12:43:48 +0500

    navigating by using js from within component

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button class="btn btn-primary" @click="backToHome">Back to Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods:{
+      backToHome(){
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
