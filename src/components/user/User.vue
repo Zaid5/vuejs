20d0be56d170bf8bd310768f09ff9f99fd5a0a23
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-21 12:51:47 +0500

    fetching dynamic routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,16 +2,22 @@
   <div>
     <h1>The User Page</h1>
     <hr>
+    <p>Loaded Id: {{id }}</p>
     <button class="btn btn-primary" @click="backToHome">Back to Home</button>
   </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return{
+        id: this.$route.params.id
+      }
+    },
     methods:{
       backToHome(){
         this.$router.push('/');
       }
-    }
+    },
   }
 </script>
