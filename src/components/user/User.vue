ddef36bee2e55b2579e30b67822ff5c233139d9f
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-21 15:53:43 +0500

    adding named routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -11,7 +11,7 @@
   export default {
     methods:{
       backToHome(){
-        this.$router.push('/');
+        this.$router.push({name: 'home' });
       }
     },
   }
--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,7 +2,8 @@
   <div>
     <h3>Some User Details</h3>
     <p>User has id: {{$route.params.id}}</p>
-    <router-link tag="button" :to="'/user/'+$route.params.id+ '/edit'"
+    <router-link tag="button"
+                 :to="{name: 'userEdit', params: { id: $route.params.id }}"
                  class="btn btn-primary">
       Edit User
     </router-link>
