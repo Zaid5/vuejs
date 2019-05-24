52ccb5391c11489c003c3f615860ea4ad4f04ca3
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-05-24 12:32:25 +0500

    controlling scroll behaviour

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   routes,    //router: router
   mode: 'history',
+  scrollBehavior(to,from,savedPosition){
+    if (savedPosition){
+      return{savedPosition};
+    }
+    if(to.hash){
+      return {
+        selector: to.hash
+      };
+    }
+    return{
+      x:0,
+      y:700
+    };
+  },
 });
 
 new Vue({
