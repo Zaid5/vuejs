6f328a14462fb66765c25ee17f96047b7356e0f7
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-06-12 16:56:19 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <app-user></app-user>
-            </div>
-        </div>
-    </div>
+  <div class="container">
+
+  </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
 
-    export default {
-        components: {
-            appUser: User
-        }
-    }
+  export default {
+
+  }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
+
 </style>
