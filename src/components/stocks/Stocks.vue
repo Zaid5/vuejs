5652509fa4c3352b2ea0b208f9bb16fffebd83d7
Zaid Iqbal <zaid.iqbal@teknuk.com>
2019-06-12 20:10:34 +0500

    creating stock component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Stocks</h1>
+  <div>
+    <app-stock v-for="stock in stocks"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+  export default {
+    data(){
+      return{
+        stocks: [
+          {id: 1, name: 'BMW', price: 11},
+          {id: 2, name: 'Google', price: 13},
+          {id: 3, name: 'Facebook', price: 15},
+          {id: 4, name: 'Descon', price: 16},
+        ]
+      }
+    },
+    components:{
+      appStock: Stock
+    }
+  }
+</script>
